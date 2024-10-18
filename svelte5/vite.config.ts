71c5d084d96138c1619fcb63beb24c9b652ca6a0
zapaz.eth <alain@kredeum.com>
2024-10-18 19:22:15 +0200

    refacto & updates

--- svelte5/vite.config.ts
+++ svelte5/vite.config.ts
@@ -1,5 +1,7 @@
 import { sveltekit } from "@sveltejs/kit/vite";
 import { defineConfig } from "vitest/config";
+// import { nodePolyfills } from "vite-plugin-node-polyfills";
+// plugins: [sveltekit(), nodePolyfills()],
 
 export default defineConfig({
   plugins: [sveltekit()],
